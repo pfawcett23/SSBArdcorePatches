42bdcdfac5cb53d6f557ac39d11a643016ab0152
Peter Fawcett <pfawcett23@gmail.com>
2014-01-01 17:19:59 -0800

    Update to ssbMonoADSR
    Updated code to use same methods/logic as the ADSR_EX code.
    Fixed A number of bugs.
    Updated the timing calculation code for attack, decay and release.

--- ssbMonoADSR/ssbMonoADSR.ino
+++ ssbMonoADSR/ssbMonoADSR.ino
@@ -1,42 +1,52 @@
-// Program: ssbMonoADSR
-// Ccurrently the sustain value is fixed in the program, (modify variable 'sustainAmount' below.
-// Attack (A0), Decay (A1) and Release (A3) are mapped to knobs. 
-// Knob A2 can be used as a manual gate if nothing is plugged into A2.
-//
-// Note: Monophonic. Can handle only one note gate at a time. If it get's more than one, it WILL
-//       produce unpredictable results.
-//
-//  I/O Usage:
-//    Knob 1: Attack
-//    Knob 2: Decay
-//    Knob 3/Jack A2: GATE INPUT
-//    Knob 4/Jack A3: RELEASE KNOB AND/OR RELEASE CV
-//    Digital Out 1:
-//    Digital Out 2: 
-//    Clock In:
-//    Analog Out:ADSR OUT
-//  Input Expander: unused
-//  Output Expander:unused
-//
-//  Created:  FEB 2012 by Dan Snazelle
-//  Adapted:  Nov 2013 by Peter Fawcett
-//  
-//  ============================================================
-//
-//  License:
-//
-//  This software is licensed under the Creative Commons
-//  "Attribution-NonCommercial license. This license allows you
-//  to tweak and build upon the code for non-commercial purposes,
-//  without the requirement to license derivative works on the
-//  same terms. If you wish to use this (or derived) work for
-//  commercial work, please contact 20 Objects LLC at our website
-//  (www.20objects.com).
-//
-//  For more information on the Creative Commons CC BY-NC license,
-//  visit http://creativecommons.org/licenses/
-//
-//
+/* Program: ssbMonoADSR
+ *
+ * This is an adaptation of the ADSR_ENV from the SnazzyFX libarary.
+ *
+ * Attack (A0), Decay (A1), Gate (A2) and Release (A3).
+ * Gate and release should be CV controlable.
+ *
+ * Ccurrently the sustain value is fixed in the program, (modify variable 'sustainAmount' below.
+ * Attack (A0), Decay (A1) and Release (A3) are mapped to knobs. 
+ * Knob A2 can be used as a manual gate if nothing is plugged into A2.
+ *
+ * Note: Monophonic. Can handle only one note gate at a time. If it get's more than one, it WILL
+ *       produce unpredictable results.
+ *
+ *  I/O Usage:
+ *    Knob 1:          Attack
+ *    Knob 2:          Decay
+ *    Knob 3/Jack A2:  Gate (knob and/or CV)
+ *    Knob 4/Jack A3:  Release (knob and/or CV)
+ *    Digital Out 1:   Unused
+ *    Digital Out 2:   Unused 
+ *    Clock In:        Unused
+ *    Analog Out:      ADSR OUT
+ *  Input Expander: 
+ *    Knob A4/Jack A4: Unused
+ *    Knob A5/Jack A5: Unused
+ *  Output Expander:
+ *    Bits 0-7:        Each bit of the envelope
+ *
+ *  Created:  FEB 2012 by Dan Snazelle
+ *  Adapted:  Nov 2013 by Peter Fawcett
+ *            Jan 01 2014 - Updated code to use same methods/logic as the ADSR_EX code.
+ *                        - Fixed A number of bugs.
+ *                        - Updated the timing calculation code for attack, decay and release.
+ *  ============================================================
+ *
+ *  License:
+ *
+ *  This software is licensed under the Creative Commons
+ *  "Attribution-NonCommercial license. This license allows you
+ *  to tweak and build upon the code for non-commercial purposes,
+ *  without the requirement to license derivative works on the
+ *  same terms. If you wish to use this (or derived) work for
+ *  commercial work, please contact Peter Fawcett at our website
+ *  (www.SoundSweepsBy.com).
+ *
+ *  For more information on the Creative Commons CC BY-NC license,
+ *  visit http://creativecommons.org/licenses/
+ */
 
 // Envelope States:
 const int     ATTACK       = 0;
@@ -45,14 +55,16 @@ const int     SUSTAIN      = 2;
 const int     RELEASE      = 3;
 
 const int     ENVELOPE_MAX = 1023;
-
+const boolean OFF          = 0;
+const boolean ON           = 1;
+const int     GATE_ON      = 120;  // Amount above which gate is considered on.
+const int     GATE_OFF     = 100;  // Amount below which gate is considered off.
+                                   // Set Gate off lower to help prevent some jitter.
 //  constants related to the Arduino Nano pin use
 const int     pinOffset    = 5;       // DAC     -> the first DAC pin (from 5-12)
 
-// Adjust for taste
-int gateValueoldOn  = 120;
-int gateValueoldOff = 100;
-int sustainAmount = 70;        // Precent int value 0 - 100.
+// EDIT THIS VALUE TO CHANGE SUSTAIN AMOUNT!!!!
+int           SUSTAIN_AMOUNT = 70;    // Percent value (int)  0 - 100.
 
 // Envelope State:
 // - 0 : attack phase     [gate on       -> max envelope  (or gate off)]
@@ -60,20 +72,11 @@ int sustainAmount = 70;        // Precent int value 0 - 100.
 // - 2 : sustain phase    [sustain level -> gate off                   ]
 // - 3 : release phase    [gate off      -> 0             (or gate on) ]
 int envelopeState = ATTACK;
-
-/* Un-Comment For Debugging
-// Debugging
-const boolean FALSE        = 0;
-const boolean TRUE         = 1;
-boolean debug = FALSE;
-int tick = 0;
-int tickOutCount = 50;
-*/
+// Current Gate State
+boolean gateState = OFF;
 
 // Current Envelope
-int envelopeVal = 0;
-// Current Gate Value
-int gateValue = 0;
+float envelopeVal = 0.0;
 
 // Envelope Values
 float attackValue  = 0.0;
@@ -81,20 +84,14 @@ float decayValue   = 0.0;
 float sustainValue = 0.0;
 float releaseValue = 0.0;
 
-
-//  ==================== setup() START ======================
-//
-//  Setup patch. Enable state of pins as needed.
-//  This code will run once at start of patch, right after load.
-//
+/*  ==================== setup() START ======================
+ *
+ *  Setup patch. Enable state of pins as needed.
+ *  This code will run once at start of patch, right after load.
+ */
 void setup()
 {
-    /* Un-Comment For Debugging.
-    if (debug)
-    {
-        Serial.begin(9600);
-    }
-    */
+    
     // set up the 8-bit DAC output pins
     for (int i = 0; i < 8; i++)
     {
@@ -105,24 +102,23 @@ void setup()
 //  ==================== setup() END =======================
 
 
-//  ==================== loop() START =======================
-//
-//  Master Loop.
-//  Loop will be called over and over with out pause.
-//  Main logic of patch.
-//
+/*  ==================== loop() START =======================
+ *
+ *  Master Loop.
+ *  Loop will be called over and over with out pause.
+ *  Main logic of patch.
+ */
 void loop()
 {
-    /* gateValue:
-           - if A2 connected to note on/off gate
-               Will toggle between 0 and the max for the gate (941 in my case).
-               A2 Knob will adjust gate amount.
-               Note if it is too low, the gate will not trigger.
-           - If A2 is not connected to a gate signal.
-               Knob will adjust gate amount between 0 and 1023.
+    /*
+       - if A2 connected to note on/off gate
+           Will toggle between 0 and the max for the gate (941 in my case).
+           A2 Knob will adjust gate amount.
+           Note if it is too low, the gate will not trigger.
+       - If A2 is not connected to a gate signal.
+           Knob will adjust gate amount between 0 and 1023.
     */
-    gateValue = analogRead(2);
-
+    gateState = gate_state(gateState, analogRead(2));
     /*
         get current state of controls.
         - Note that attack, decay and release are the ammounts to add to/subtract from
@@ -132,87 +128,133 @@ void loop()
         - But sustain is not an increment. it's a fixed value to sustain at. Thus
           use the sustainBase (for now, may use gateValue in future)
     */
-    attackValue = 255.0 / (analogRead(0) + 5);
-    decayValue = 255.0 / (analogRead(1) + 5);
-    sustainValue = ENVELOPE_MAX * (float)(sustainAmount / 100.0);
-    releaseValue = 255.0 / (analogRead(3) + 5);
-  
-    if (gateValue > gateValueoldOn)
+    attackValue = map_float(analogRead(0), 0, 1023, 204.6, .5);
+    decayValue = map_float(analogRead(1), 0, 1023, 204.6, .5);
+    sustainValue = (int)(ENVELOPE_MAX * (float)(SUSTAIN_AMOUNT / 100.0));
+    releaseValue = map_float(analogRead(3), 0, 1023, 204.6, .5);
+    
+    int envLoopState = ATTACK;
+
+    if (gateState == ON)
     {
         if (envelopeState == RELEASE)
         {
-            envelopeState == ATTACK;  // We were re-triggered before the end of the release.
+            envLoopState = ATTACK;
         }
-        if (envelopeState == ATTACK) // Attack
+        if (envelopeState == ATTACK)     // Attack
         {
-            envelopeVal = (envelopeVal + attackValue); //step up to full level
-            if (envelopeVal >= ENVELOPE_MAX)
-            {
-                envelopeState = DECAY; // Attack Over, State Set to Decay.
-                envelopeVal = ENVELOPE_MAX;
-            }
-        }    //switch to decay
+            envLoopState = ATTACK;
+            envelopeVal = do_attack_inc(envelopeVal, attackValue);
+        }
         else if (envelopeState == DECAY) // Decay
         {
-            if (envelopeVal > sustainValue) //stop at sustain level
-            {
-                envelopeVal = (envelopeVal - decayValue);
-            }
-            else
-            {
-                if (envelopeVal <= 0)  // Handle sustain set to 0.
-                {
-                    envelopeVal = 0;
-                    envelopeState = ATTACK; // reset for next gate on.
-                }
-                else
-                {
-                    envelopeState = SUSTAIN;
-                    envelopeVal = sustainValue;
-                }
-            }
+            envLoopState = DECAY;
+            envelopeVal = do_decay_dec(envelopeVal, decayValue, sustainValue);
         }
-        else
+        else                             // Sustain
         {
+            envLoopState = SUSTAIN;
             envelopeVal = sustainValue;
         }
     }
-    else if (gateValue < gateValueoldOff)
+    else                                 // Release
     {
-        if (envelopeState != RELEASE)
-        {
-            envelopeState = RELEASE;
-        }
-        if (envelopeVal > 0)
-        {
-            envelopeVal = envelopeVal - releaseValue; //gate is done, do the release
-        }
-        else
-        {
-            envelopeVal = 0;
-            envelopeState = ATTACK; // reset for next gate on.
-        }
+        envLoopState = RELEASE;
+        envelopeVal = do_release_dec(envelopeVal, releaseValue);
     }
 
-    dacOutput((envelopeVal >> 2));
+    // Keep track of state based on envelope, current state and gate state.
+    envelopeState = envelope_state(envelopeState, envLoopState, envelopeVal, sustainValue);
 
-    /* Un-Comment For Debugging
-    if (debug == TRUE)
-    {
-        if (tick == tickOutCount)
-        {
-            Serial.print("Envelope Value      => ");
-            Serial.println(envelopeVal);
-            Serial.println("");
-            tick = 0;
-        }
-        tick++;
-    }
-    */
+    // Write envelope to DAC.
+    dacOutput(((long)envelopeVal >> 2));
 }
 
 
 //  ==================== loop() END =======================
+
+float map_float(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+boolean gate_state(boolean gate, int currentGate)
+{
+    if (currentGate > GATE_ON)
+    {
+        gate = ON;
+    }
+    else if (currentGate < GATE_OFF)
+    {
+        gate = OFF;
+    }
+    return gate;
+}
+
+int envelope_state(int state, int currentState, float envelopeVal, float sustainVal)
+{
+    if (envelopeVal >= ENVELOPE_MAX)
+    {
+        state = DECAY;
+    }
+    else if ((state == DECAY) && (envelopeVal <= sustainVal))
+    {
+        state = SUSTAIN;
+    }
+    else if (envelopeVal <= 0)
+    {
+        state = ATTACK;
+    }
+    else if (state != currentState)
+    {
+        state = currentState;
+    }
+    return state;
+}
+
+float do_attack_inc(float envelopeVal, float attackValue)
+{
+    envelopeVal = (envelopeVal + attackValue); //step up to full level
+    if (envelopeVal >= ENVELOPE_MAX)
+    {
+        envelopeVal = ENVELOPE_MAX;
+    }
+    return envelopeVal;
+}
+
+float do_decay_dec(float envelopeVal, float decayValue, float sustainValue)
+{
+    if (envelopeVal > sustainValue) // If greater than sustain
+    {
+        // Decriment by decay value.
+        envelopeVal = (envelopeVal - decayValue);
+    }
+    // if envelope is < 0 set to 0. Shouldn't get here unless sustain is 0.
+    if (envelopeVal <= 0)
+    {
+        envelopeVal = 0;
+    }
+    else if (envelopeVal < sustainValue)
+    {
+        // if current envelope got set lower than sustain, set it to sustain.
+        envelopeVal = sustainValue;
+    }
+    return envelopeVal;
+}
+
+float do_release_dec(float envelopeVal, float releaseValue)
+{
+    if (envelopeVal > 0)
+    {
+        // calculate new value of releasse envelope.
+        envelopeVal = envelopeVal - releaseValue;
+    }
+    if (envelopeVal < 0)
+    {
+        envelopeVal = 0; // if below 0, set to 0.
+    }
+    return envelopeVal;
+}
 
 //  =================== convenience routines ===================
 
